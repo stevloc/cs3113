fb1b08e7262b8a9e125ec6c270bab84825c5beef
Steven Lo <143578058+stevloc@users.noreply.github.com>
2024-03-23 22:43:35 -0400

    Added HW04 to repo

--- hw/sl9820_hw04/SDLProject/Scene.cpp
+++ hw/sl9820_hw04/SDLProject/Scene.cpp
@@ -1,1 +1,11 @@
+/**
+* Author: Steven Lo
+* Assignment: Rise of the AI
+* Date due: 2024-03-23, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "Scene.h"
--- hw/sl9820_hw04/SDLProject/Utility.cpp
+++ hw/sl9820_hw04/SDLProject/Utility.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Steven Lo
+* Assignment: Rise of the AI
+* Date due: 2024-03-23, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define LOG(argument) std::cout << argument << '\n'
 #define STB_IMAGE_IMPLEMENTATION
 #define NUMBER_OF_TEXTURES 1
