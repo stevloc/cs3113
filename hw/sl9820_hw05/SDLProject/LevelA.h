00b650b0973aaaf4b10f09cfe1862e75a30a7a15
Steven Lo <143578058+stevloc@users.noreply.github.com>
2024-04-13 23:17:58 -0400

    Update LevelA.h

--- hw/sl9820_hw05/SDLProject/LevelA.h
+++ hw/sl9820_hw05/SDLProject/LevelA.h
@@ -1,3 +1,12 @@
+/**
+* Author: Steven Lo
+* Assignment: Platformer
+* Date due: 2024-04-13, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #include "Scene.h"
 
 class LevelA : public Scene {
